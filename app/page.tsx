5eb9c0d3f4f4f6ce80e57697b2e8e3f9fb4a98b1
trvankiet02 <trvankiet02@gmail.com>
2024-10-18 11:07:38 +0700

    complete tutorial

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import styles from "@/app/ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
 import heroDesktop from "@/public/hero-desktop.png";
 import heroMobile from "@/public/hero-mobile.png";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default function Page() {
   return (
